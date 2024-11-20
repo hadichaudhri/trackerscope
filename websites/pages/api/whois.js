634800a5ca8be4ac508009bdd2c2c002b2b625bc
Yihong Song <ys377@duke.edu>
2024-11-20 02:57:15 -0500

    new whois

--- websites/pages/api/whois.js
+++ websites/pages/api/whois.js
@@ -1,21 +1,27 @@
-import whois from "whois-ux";
+import whois from "whois-json";
 
 export default async (req, res) => {
-    // Get the ip from the query string
-    const ip = req.query.ip;
-    console.log("IP Address:", ip);
+    try {
+        // const { ip } = req.query;
+        const ip = req.query.ip;
 
-    // Get the WHOIS data for the IP address
-    whois.whois(ip, function (err, data) {
-        if (err) {
-            console.error(err);
-            res.status(500).json({
-                error: "Internal Server Error",
-                details: err.message,
-            });
-            return;
+        // Validate IP
+        if (!ip || !isValidIP(ip)) {
+            return res.status(400).json({ error: "Invalid IP address" });
         }
-        console.log(JSON.stringify(data));
-        res.status(200).json(data);
-    });
+
+        // Perform WHOIS lookup
+        const whoisData = await whois(ip);
+        return res.status(200).json(whoisData);
+    } catch (err) {
+        console.error("Error in WHOIS handler:", err);
+        return res.status(500).json({ error: "Internal Server Error" });
+    }
 };
+
+function isValidIP(ip) {
+    // Simple IP validation (IPv4 example)
+    return /^(25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)\.(\d{1,3}\.){2}\d{1,3}$/.test(
+        ip
+    );
+}
